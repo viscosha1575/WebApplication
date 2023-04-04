a1fc1a708c6c0831da349784bfedb9d55da91e64
viscosha1575 <viscosha1575@gmail.com>
2023-04-04 14:20:31 +0300

    First commit

---
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+
+    }
+}
